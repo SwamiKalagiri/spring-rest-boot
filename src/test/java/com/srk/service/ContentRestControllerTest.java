51babbc65d1d3378cff8017c7652e4f89a21e270
Swami Kalagiri <KalagiriSwamiReddy@JohnDeere.com>
2016-11-17 16:23:26 -0600

    Added the test.

---
+++ src/test/java/com/srk/service/ContentRestControllerTest.java
@@ -0,0 +1,27 @@
+package com.srk.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ContentRestControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void shouldGetContent() throws Exception {
+        String expectedResponse = "{\"id\":1,\"content\":\"Welcome to Rest World!\"}";
+
+        String actualResponse = restTemplate.getForObject("/getContent", String.class);
+
+        assertThat(actualResponse).isEqualTo(expectedResponse);
+    }
+
+}
