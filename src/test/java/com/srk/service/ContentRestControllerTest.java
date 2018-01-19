d157be18f5868c39f28013cb6c81e65f5fa139df
Swami Kalagiri <swami.kalagiri@gmail.com>
2018-01-18 21:18:00 -0600

    Updated test and gitignore

--- src/test/java/com/srk/service/ContentRestControllerTest.java
+++ src/test/java/com/srk/service/ContentRestControllerTest.java
@@ -5,9 +5,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collections;
+import java.util.Locale;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -17,7 +22,14 @@ public class ContentRestControllerTest {
 
     @Test
     public void shouldGetContent() throws Exception {
-        String expectedResponse = "{\"id\":1,\"content\":\"Welcome to Rest World!!! Local is en_US\"}";
+        String expectedResponse = "{\"id\":1,\"content\":\"Welcome to Rest World!!! Local is it_IT\"}";
+
+        restTemplate.getRestTemplate().setInterceptors(
+            Collections.singletonList((request, body, execution) -> {
+            request.getHeaders()
+                    .add("Accept-Language", Locale.ITALY.toLanguageTag());
+            return execution.execute(request, body);
+        }));
 
         String actualResponse = restTemplate.getForObject("/getContent", String.class);
 
